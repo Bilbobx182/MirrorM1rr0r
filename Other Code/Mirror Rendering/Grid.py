cdbf273b397b58481a7f16bc859e566e49eacf76
bilbobx182 <c14474048@mydit.ie>
2017-11-07 10:52:50 +0000

    Improved failure rate of AWS getMessage

--- Other Code/Mirror Rendering/Grid.py
+++ Other Code/Mirror Rendering/Grid.py
@@ -1,3 +1,4 @@
+from kivy import Config
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.button import Label, Button
@@ -15,22 +16,26 @@ class MyApp(App):
         lm_json = requests.get(url).json()
         return lm_json['0']['Contents']
 
+    def hello(self):
+        print("hello")
     def build(self):
-        Window.fullscreen = 'auto'
-        # return Label(text="hello", pos=((1920/2) *-.98, 0))
+        Config.set('graphics', 'width', '500')
+        Config.set('graphics', 'height', '500')
+
+
         layout = GridLayout(cols=3, rows=3)
 
-        layout.add_widget(Button(text='Side Top LEFT'))
-        layout.add_widget(Button(text='CT'))
+        layout.add_widget(AsyncImage(source=MyApp.getInformationFromQueue(self)))
+        layout.add_widget(Label(text='CT'))
 
-        layout.add_widget(Button(text='Side Top RIGHT'))
-        layout.add_widget(Button(text='CL'))
+        layout.add_widget(Label(text='Side Top RIGHT'))
+        layout.add_widget(Label(text='CL'))
 
-        layout.add_widget(Button(text='CM'))
-        layout.add_widget(Button(text='CR'))
+        layout.add_widget(Label(text='CM'))
+        layout.add_widget(Label(text='CR'))
 
-        layout.add_widget(Button(text='SIDE BOT LEFT'))
-        layout.add_widget(Button(text='CB'))
+        layout.add_widget(Label(text='SIDE BOT LEFT'))
+        layout.add_widget(Label(text='CB'))
         layout.add_widget(AsyncImage(source=MyApp.getInformationFromQueue(self)))
         return layout
 
--- Other Code/Mirror Rendering/figureStuffOutClass.py
+++ Other Code/Mirror Rendering/figureStuffOutClass.py
@@ -1,8 +1,45 @@
-class display():
-    def getMessage(self):
-        return "hello"
+import boto3
 
-    def start(self):
-        print(display.getMessage())
+import json
 
-display.start()
+def getMessageFromQueue(messageCountRequested, queue):
+    maxcount = int(messageCountRequested)
+    count = 0
+    messageItem = {}
+
+    while (count < maxcount):
+        response = sqs.receive_message(
+            QueueUrl=queue,
+            AttributeNames=[
+                'SentTimestamp'
+            ],
+            MaxNumberOfMessages=maxcount,
+            MessageAttributeNames=[
+                'All'
+            ],
+            VisibilityTimeout=0,
+            WaitTimeSeconds=0
+        )
+        messageItem[str(count)] = {
+            "Contents": response['Messages'][0]['Body']
+        }
+        count += 1
+    return (messageItem)
+
+
+def lambda_handler():
+    out = {}
+
+    messageCountRequested = 2
+    queue = 'https://sqs.eu-west-1.amazonaws.com/186314837751/ciaran'
+    contents = json.dumps(getMessageFromQueue(messageCountRequested, queue))
+    out['statusCode'] = 200
+    out['body'] = contents
+
+    out['headers'] = {
+        "content-type": "application-json"
+    }
+
+    return (out)
+
+print(lambda_handler())
