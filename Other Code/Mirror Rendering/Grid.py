31b6a011bc492de2805e8c78adb58605d982899c
bilbobx182 <c14474048@mydit.ie>
2017-11-14 19:56:47 +0000

    sorted from queue working

--- Other Code/Mirror Rendering/Grid.py
+++ Other Code/Mirror Rendering/Grid.py
@@ -15,23 +15,21 @@ class MyApp(App):
     def getInformationFromQueue(self):
         url = "https://tj5ur8uafi.execute-api.us-west-2.amazonaws.com/Prod/getmessage?queue=https://sqs.eu-west-1.amazonaws.com/186314837751/hellociaran&count=2"
         lm_json = requests.get(url).json()
-        duct = lm_json['0']['Contents']
 
         contents = []
-        for key, value in duct.items():
+        for key, value in lm_json.items():
             item = {}
-            item['Message'] = value['Message']
+            item['Message'] = value['Contents']
             item['Timestamp'] = int(value['SentTimestamp'])
             contents.append(item);
 
-        return (contents.sort(key=operator.itemgetter('Message', 'Timestamp')))
+        return sorted(contents, key=operator.itemgetter('Message', 'Timestamp'))
 
     def hello(self):
         print("hello")
 
     def build(self):
         layout = GridLayout(cols=3, rows=3)
-
 
         result = self.getInformationFromQueue()
 
@@ -51,6 +49,7 @@ class MyApp(App):
         layout.add_widget(Label(text=''))
 
         return layout
+
 
 Window.fullscreen = 'auto'
 MyApp().run()
