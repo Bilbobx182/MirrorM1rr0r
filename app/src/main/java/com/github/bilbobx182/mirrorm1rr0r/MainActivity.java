457f98629ce3daf7eb83242d1151aee01486cbfa
bilbobx182 <c14474048@mydit.ie>
2017-11-15 13:24:20 +0000

    Android to SQS implemented via API Gateway

--- app/src/main/java/com/github/bilbobx182/mirrorm1rr0r/MainActivity.java
+++ app/src/main/java/com/github/bilbobx182/mirrorm1rr0r/MainActivity.java
@@ -1,31 +1,24 @@
 package com.github.bilbobx182.mirrorm1rr0r;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.AsyncTask;
-import android.os.Process;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import org.w3c.dom.Text;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-import static rx.schedulers.Schedulers.start;
 
 
 public class MainActivity extends AppCompatActivity {
 
     String requestResponse;
     TextView queryResult;
+    EditText queryInputEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,42 +27,41 @@ public class MainActivity extends AppCompatActivity {
         Button doneButton = (Button) findViewById(R.id.commitButton);
         queryResult = (TextView) findViewById(R.id.queryResponseTextView);
         doneButton.setOnClickListener(v -> {
+            queryInputEditText = (EditText) findViewById(R.id.queryEditText);
+            String input = queryInputEditText.getText().toString();
+
+            Log.v("EditText", input);
+
+            String urlToPass = "https://tj5ur8uafi.execute-api.us-west-2.amazonaws.com/Prod/sendmessagetoqueue?queueurl=https://sqs.eu-west-1.amazonaws.com/186314837751/normalQueue&message=" + input;
             BackgroundThread thread = new BackgroundThread();
-            thread.execute("TEST");
+            thread.execute(urlToPass);
         });
     }
 
-    private class BackgroundThread extends AsyncTask<String,String, String>
-    {
+    private class BackgroundThread extends AsyncTask<String, String, String> {
 
         @Override
-        protected void onPreExecute()
-        {
+        protected void onPreExecute() {
             super.onPreExecute();
         }
 
         @Override
-        protected String doInBackground(String... params)
-        {
+        protected String doInBackground(String... params) {
             String result = "";
 
             URL url;
             HttpURLConnection urlConnection = null;
             try {
-                url = new URL("https://swapi.co/api/people/?search=luke&format=json");
-
-                urlConnection = (HttpURLConnection) url
-                        .openConnection();
-
+                url = new URL(params[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
                 InputStream in = urlConnection.getInputStream();
-
                 InputStreamReader isw = new InputStreamReader(in);
 
                 int data = isw.read();
                 while (data != -1) {
                     char current = (char) data;
                     data = isw.read();
-                    result+=current;
+                    result += current;
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -82,22 +74,18 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        protected void onProgressUpdate(String...values)
-        {
+        protected void onProgressUpdate(String... values) {
             super.onProgressUpdate(values);
         }
+
         // This runs in UI when background thread finishes
         @Override
-        protected void onPostExecute(String result)
-        {
+        protected void onPostExecute(String result) {
             queryResult.append(result);
             super.onPostExecute(result);
         }
     }
 }
-
-
-
 
 
 //    private String validateInput() {
