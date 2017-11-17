41c7351c518054e9454a3d9f4e26ecd54f5871da
bilbobx182 <c14474048@mydit.ie>
2017-11-17 19:05:41 +0000

    For some reason it can only request 1 per loop

--- Other Code/Lambda Methods/getFIFOMessage.py
+++ Other Code/Lambda Methods/getFIFOMessage.py
@@ -1,64 +1,66 @@
 import json
 import boto3
 
-sqs = boto3.client('sqs', region_name='eu-west-1')
+sqs = boto3.client('sqs', region_name='eu-west-1',
+                   aws_access_key_id='AKIAIDMELKX2YW6VOJ7Q',
+                   aws_secret_access_key='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')
 
 
-def getMessageFromQueue(messageCountRequested, queue):
-    maxcount = int(messageCountRequested)
-    count = 0
-    receipts = []
+def getMessageFromQueue(queue, currentItem):
     outputJSON = {}
 
     # Receive message from SQS queue
-    while (count < maxcount):
-        response = sqs.receive_message(
-            QueueUrl=queue,
-            MaxNumberOfMessages=maxcount,
-            AttributeNames=[
-                'SentTimestamp'
-            ],
-            MessageAttributeNames=[
-                'All'
-            ],
-        )
+    response = sqs.receive_message(
+        QueueUrl=queue,
+        MaxNumberOfMessages=1,
+        AttributeNames=[
+            'All'
+        ],
+        MessageAttributeNames=[
+            'All'
+        ],
+    )
 
-        outputJSON[str(count)] = {
-            "Contents": response['Messages'][0]['Body'],
-            "SentTimestamp": response['Messages'][0]['Attributes']['SentTimestamp']
-        }
+    outputJSON[str(currentItem)] = {
+        "Contents": response['Messages'][0]['Body'],
+        "SentTimestamp": response['Messages'][0]['Attributes']['SentTimestamp']
+    }
 
-        message = response['Messages'][0]
-        receipt_handle = message['ReceiptHandle']
+    message = response['Messages'][0]
+    receipt_handle = message['ReceiptHandle']
 
-        # Change visibility timeout of message from queue
-        sqs.change_message_visibility(
-            QueueUrl=queue,
-            ReceiptHandle=receipt_handle,
-            VisibilityTimeout=36000
-        )
-        deleteResult = sqs.delete_message(QueueUrl=queue, ReceiptHandle=receipt_handle)
-        print('Received and changed visibilty timeout of message: %s' % message)
-        if (response['Messages'][0]['ReceiptHandle'] not in receipts):
-            receipts.append(response['Messages'][0]['ReceiptHandle'])
-            count += 1
+    # Change visibility timeout of message from queue
+    sqs.change_message_visibility(
+        QueueUrl=queue,
+        ReceiptHandle=receipt_handle,
+        VisibilityTimeout=36000
+    )
+    deleteResult = sqs.delete_message(QueueUrl=queue, ReceiptHandle=receipt_handle)
+
     return outputJSON
 
 
-def lambda_handler(event, context):
+def lambda_handler():
     out = {}
-    if (not event['queryStringParameters']['queue'] or not event['queryStringParameters']['count']):
-        out['statusCode'] = 400
-        out['body'] = "Queue or count not specified"
-    else:
-        messageCountRequested = event['queryStringParameters']['count']
-        queue = event['queryStringParameters']['queue']
 
-        contents = json.dumps(getMessageFromQueue(messageCountRequested, queue))
-        out['statusCode'] = 200
-        out['body'] = contents
+    messageCountRequested = 2
+    queue = 'https://sqs.eu-west-1.amazonaws.com/186314837751/ciaranVis.fifo'
+
+    result = {}
+
+    loopCount = 0
+
+    while (loopCount < messageCountRequested):
+        result[loopCount] = getMessageFromQueue(queue, messageCountRequested)
+        loopCount += 1
+
+    out['statusCode'] = 200
+    out['body'] = result
 
     out['headers'] = {
         "content-type": "application-json"
     }
     return (out)
+
+
+print(lambda_handler())
