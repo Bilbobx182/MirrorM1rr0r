20195d92935ee72f0b574a51eca489034242892a
bilbobx182 <c14474048@mydit.ie>
2017-11-27 09:27:53 +0000

    Updated python code for the demo

--- Other Code/Mirror Rendering/MirrorGridRendering.py
+++ Other Code/Mirror Rendering/MirrorGridRendering.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.uix.gridlayout import GridLayout
 from kivy.clock import Clock
 from kivy.uix.image import AsyncImage
@@ -13,11 +14,10 @@ widgetsToRender = [
     [" ", " ", " "],
     [" ", " ", " "],
     [" ", " ", " "]]
-
-messageCount = 3
+messageCount = 1
 base = "https://tj5ur8uafi.execute-api.us-west-2.amazonaws.com/Prod/getfifomessage"
 queue = "?queueurl=https://sqs.eu-west-1.amazonaws.com/186314837751/ciaranVis.fifo"
-count = "&count=" + messageCount
+count = "&count=" + str(messageCount)
 
 
 # result = requests.get(base + queue + count).json()
@@ -25,26 +25,26 @@ count = "&count=" + messageCount
 
 def performRequest():
     result = requests.get(base + queue + count).json()
-    messageCountLoop = 0
-    while(messageCountLoop < messageCount):
-        messageKey = 'Message ' + messageCountLoop
-        if (messageKey in result):
 
-            contents = json.loads(result[messageKey]['Contents'])
+    messageKey = 'Message 0'
+    if (messageKey in result):
 
-            if ('location' in contents):
-                location = contents['location']
-                yLocation = int(contents['location'].split(",")[0])
-                xLocation = int(contents['location'].split(",")[1])
+        contents = json.loads(result[messageKey]['Contents'])
 
-                isOccupied[yLocation][xLocation] = True
-                widgetsToRender[yLocation][xLocation] = contents['messagePayload']
+        if ('location' in contents):
+            location = contents['location']
+            yLocation = int(contents['location'].split(",")[0])
+            xLocation = int(contents['location'].split(",")[1])
 
-            # If no location is specified default it to the center.
-            elif (isOccupied[1][1] == False):
-                widgetsToRender[1][1] = contents['messagePayload']
+            isOccupied[yLocation][xLocation] = True
+            widgetsToRender[yLocation][xLocation] = contents['messagePayload']
 
-    print(isOccupied)
+        # If no location is specified default it to the center.
+        elif (isOccupied[1][1] == False):
+            widgetsToRender[1][1] = contents['messagePayload']
+
+
+print(isOccupied)
 
 
 def falsifyOccupied():
@@ -83,4 +83,5 @@ class MirrorApplication(App):
             obj.add_widget(Label(text=widgetToRender))
 
 
+Window.fullscreen = 'auto'
 MirrorApplication().run()
