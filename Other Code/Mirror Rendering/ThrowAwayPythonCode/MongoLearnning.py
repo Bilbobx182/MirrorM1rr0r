38c7261a44bb9385adfec668a3e71ea84ab969eb
bilbobx182 <c14474048@mydit.ie>
2018-01-27 09:50:01 +0000

    Reading and writing from mongoDB implemented

--- Other Code/Mirror Rendering/ThrowAwayPythonCode/MongoLearnning.py
+++ Other Code/Mirror Rendering/ThrowAwayPythonCode/MongoLearnning.py
@@ -7,5 +7,9 @@ db = client.SMWS
 collection = db.SMWSCollection
 
 queryResult = db.SMWSCollection.find_one()
-
 print(queryResult)
+
+# collection.insert_one({'Text': "SometextHereInsertedByPython", 'X': 1, 'Y': 1})
+# I need to itterate over the results as it returns a cursor object using the find method
+for item in collection.find({'X': 1}):
+    print(item)
