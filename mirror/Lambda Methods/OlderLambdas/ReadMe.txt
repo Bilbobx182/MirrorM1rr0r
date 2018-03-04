a69c59d6d585c55fbd26adad0ff8f2a0cbbdcbe9
bilbobx182 <c14474048@mydit.ie>
2018-03-04 13:55:09 +0000

    Cleanup of project folders

--- mirror/Lambda Methods/OlderLambdas/ReadMe.txt
+++
@@ -1,4 +0,0 @@
-These Lambdas have been moved to this Lambda graveyard folder.
-They are older methods that are no longer in active use.
-However, they may one day be of use.
-So they will remain.
--- mirror/Mirror Rendering/Code that won't be demoed/QueueListening.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-# Keeping this as I will need a dedicated class later for queue listening
-url = "https://tj5ur8uafi.execute-api.us-west-2.amazonaws.com/Prod/getmessage?count=1"
-lm_json = requests.get(url).json()
-basicURL = lm_json['0']['Contents']
-
-print(basicURL)
