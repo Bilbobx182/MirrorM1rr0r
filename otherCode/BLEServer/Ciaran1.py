629f6514006c05ac6fce9ebf71541b218efb9b86
bilbobx182 <c14474048@mydit.ie>
2018-04-13 16:00:33 +0100

    Removed un-needed items from mirror folder

--- otherCode/BLEServer/Ciaran1.py
+++
@@ -1,6 +0,0 @@
-import os
-
-print(os.getcwd())
-
-with open(os.getcwd() +"\lol.txt","a") as myfile:
-    myfile.write("lol")
--- otherCode/lambdaMethods/getNodes.py
+++ otherCode/lambdaMethods/getNodes.py
@@ -4,42 +4,44 @@ import json
 def lambda_handler(event, context):
     result = {"nodes": [
         {
-            "title": "help",
+            "Return": "List of type nodes",
             "Description": "This returns a list of nodes with descriptions",
             "Parameters": "N/A",
-            "Return": "List of type nodes"
+            "title": "getnodes"
         },
         {
-            "title": "getMessage",
+            "ParametersDesc": "A url to a queue, the count of messages wanted (optional)",
+            "Return": "Message",
             "Description": "Returns JSON for given message in the queue",
             "Parameters": "queueurl, count",
-            "ParametersDesc": "A url to a queue, the count of messages wanted (optional)",
-            "Return": "Message"
+            "title": "getmessage"
         },
         {
-            "title": "createFIFOQueue",
+            "ParametersDesc": "A a unique hashed string that will then be used as the queue name",
+            "Return": "queueurl",
             "Description": "Creates a queue in AWS SQS so that the mirror can pull from it",
             "Parameters": "queueName",
-            "ParametersDesc": "A a unique hashed string that will then be used as the queue name",
-            "Return": "queueurl"
+            "title": "createqueue"
         },
         {
-            "title": "sendFifoMessage",
+            "Return": "String of status",
             "Description": "Send a message to a queue via query parameters",
             "Parameters": "queueurl,message,location(optional),fontsize(optional),fontcolour(optional)",
+            "title": "sendmessage",
+            "ErrorDescription": "You didn't pass the query parameters of the queueurl correctly, or message correctly",
             "ParametersDesc": "the queue you want to send to, stringified message you want to send, x and y location denoted as 'x,y' ie 1,1, a numeric value for how big you want the font, Hex colour value for font colour",
-            "Return": "String of status",
             "ErrorMessage": "Queue or message payload were not passed :(",
-            "ErrorDescription": "You didn't pass the query parameters of the queueurl correctly, or message correctly"
+            "Type": "GET"
         },
         {
-            "title": "sendFIFOMessagePOST",
+            "Return": "String of status",
             "Description": "Same as sending a message except via a post request",
             "Parameters": "Sample in the github repo under PostSample.json",
+            "title": "sendmessage",
+            "ErrorDescription": "You didn't pass the query parameters of the queueurl correctly, or message correctly",
             "ParametersDesc": "queueurl holds the queue, data holds the payload that you want to send to a queue",
-            "Return": "String of status",
             "ErrorMessage": "Queue or message payload were not passed :(",
-            "ErrorDescription": "You didn't pass the query parameters of the queueurl correctly, or message correctly"
+            "Type": "POST"
         }
     ]
     }
